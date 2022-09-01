892fc7090ed853295e71507b2f1c78c2eec85023
dominic.butler <dominic.butler@caristo.com>
2022-09-01 22:22:05 +0100

    .

--- src/App.data.js
+++ src/App.data.js
@@ -20,7 +20,7 @@ export default [
     },
     {
         url: {
-            project: 'https://doms-desktop.onrender.com',
+            project: 'https://dominicbutler.work/doms-desktop',
             code: 'https://github.com/domb5041/doms-desktop',
         },
         text: 'A replica mac desktop. It works just like the real thing. Interact with folders, windows, the menu bar and more.',
