f1c4aea3473061ede1c2e3b5d86772193d28369f
dominic.butler <dominic.butler@caristo.com>
2023-01-28 20:21:53 +0000

    updated project links

--- src/App.data.js
+++ src/App.data.js
@@ -1,39 +1,38 @@
 export default [
     {
         url: {
-            cv: '/Dominic-Butler-CV.pdf',
-            linkedin: 'https://www.linkedin.com/in/dominic-butler-816b31119',
-            instagram: 'https://www.instagram.com/dombutlerpad/',
+            cv: "/Dominic-Butler-CV.pdf",
+            linkedin: "https://www.linkedin.com/in/dominic-butler-816b31119",
+            instagram: "https://www.instagram.com/dombutlerpad/",
         },
         text: "Hi, welcome to my portfolio site! The following slides link to some side projects I've had fun working on recently. You can find out more about me and my industry experience through the links above.",
-        image: 'dom.jpg',
-        color: '#FFAD99',
+        image: "dom.jpg",
+        color: "#FFAD99",
     },
     {
         url: {
-            project: 'https://dominicbutler.work/modular-clock',
-            code: 'https://github.com/domb5041/modular-clock',
+            project: "https://modular-clock.vercel.app/",
+            code: "https://github.com/domb5041/modular-clock",
         },
-        text: 'A fully customisable modular clock with an array of possible complications.',
-        image: 'clock.jpg',
-        color: '#FFD399',
+        text: "A fully customisable modular clock with an array of possible complications.",
+        image: "clock.jpg",
+        color: "#FFD399",
     },
     {
         url: {
-            project: 'https://dominicbutler.work/doms-desktop',
-            code: 'https://github.com/domb5041/doms-desktop',
+            project: "https://doms-desktop.vercel.app/",
+            code: "https://github.com/domb5041/doms-desktop",
         },
-        text: 'A replica mac desktop. It works just like the real thing. Interact with folders, windows, the menu bar and more.',
-        image: 'desktop.jpg',
-        color: '#D1FFFD',
+        text: "A replica mac desktop. It works just like the real thing. Interact with folders, windows, the menu bar and more.",
+        image: "desktop.jpg",
+        color: "#D1FFFD",
     },
     {
         url: {
-            project:
-                'https://www.youtube.com/watch?v=JChDbIOkE58&list=PLrRbyFPS5q6w8wLmKAudLnPg1L7CMv1Lw',
+            project: "https://www.youtube.com/watch?v=JChDbIOkE58&list=PLrRbyFPS5q6w8wLmKAudLnPg1L7CMv1Lw",
         },
-        text: 'I explain some music theory in a two-part animated video series. Inspired by the illustrations and presentation of Youtube channel Kurzgesagt.',
-        image: 'notecircle.jpg',
-        color: '#FF97B2',
+        text: "I explain some music theory in a two-part animated video series. Inspired by the illustrations and presentation of Youtube channel Kurzgesagt.",
+        image: "notecircle.jpg",
+        color: "#FF97B2",
     },
 ];
