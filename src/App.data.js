b72dd2fb0298aaeffb8415e49da7500959fd988e
domb5041 <33228675+domb5041@users.noreply.github.com>
2025-01-11 21:38:34 +0000

    add some new project change some colours and images

--- src/App.data.js
+++ src/App.data.js
@@ -5,25 +5,42 @@ export default [
             linkedin: "https://www.linkedin.com/in/dominic-butler-816b31119",
             instagram: "https://www.instagram.com/dombutlerpad/",
         },
-        text: "Hi, welcome to my portfolio - The following slides link to some small projects I've worked on in my own time. You can find out more about me and my industry experience through the links above.",
+        text: "Hello and welcome to my personal hub for tech demos and hobby projects. Each slide showcases something different.",
         image: "dom.jpeg",
-        color: "#FFAD99",
+        color: "#C9FCE8",
     },
     {
         url: {
             project: "https://modular-clock.vercel.app/",
             code: "https://github.com/domb5041/modular-clock",
         },
-        text: "A fully customisable modular clock with an array of possible complications.",
-        image: "clock.jpg",
-        color: "#FFD399",
+        text: "A fully customisable modular clock with an array of possible complications. Inspired by the apple watch UI.",
+        image: "clock.png",
+        color: "#FA3337",
+    },
+    {
+        url: {
+            project: "https://bookmark-manager-l9tlugegu-domb5041s-projects.vercel.app",
+            code: "https://github.com/domb5041/bookmark-manager",
+        },
+        text: "A basic url bookmark manager featuring tagging, sorting and search. It has a backend which stores data. The UI has light/dark mode and accent theming.",
+        image: "bookmarks.png",
+        color: "#B28FFF",
+    },
+    {
+        url: {
+            project: "https://19-galaxy-generator-zeta.vercel.app",
+        },
+        text: "A three.js experiment using 3D paricles to simulate a galaxy system.",
+        image: "galaxy.png",
+        color: "#9196FB",
     },
     {
         url: {
             project: "https://doms-desktop.vercel.app/",
             code: "https://github.com/domb5041/doms-desktop",
         },
-        text: "A replica mac desktop. It works just like the real thing. Interact with folders, windows, the menu bar and more.",
+        text: "A replica mac desktop. Demonstrating basic window management and dynamic menus.",
         image: "desktop.jpg",
         color: "#D1FFFD",
     },
